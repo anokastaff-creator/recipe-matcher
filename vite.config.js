6e89b96133d867e1a483e3d7ea3b745376022a7f
anokastaff-creator <anokastaffmail@gmail.com>
2026-01-12 20:38:19 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-base: "/recipe-matcher/"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/recipe-matcher/"
 })
