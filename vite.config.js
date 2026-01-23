ee51b33a9b1d85522a4f740dd441fd825d5a795c
anokastaff-creator <anokastaffmail@gmail.com>
2026-01-23 01:45:45 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/recipe-matcher/"
+  base: '/', // Explicitly tells Vite to build for the root domain, fixing the path issue
 })
